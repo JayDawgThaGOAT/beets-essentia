621603139a2bd1cecce152a14c7f6045f83c6350
Auridh <auridh@auridh.me>
2025-08-26 17:51:53 +0200

    :sparkles: Allow mood tag writing in tests

--- test/test.py
+++ test/test.py
@@ -1,10 +1,21 @@
 import logging
 import sys
 
+import mediafile
 from beets.library import Item
+from confuse import ConfigView
 
 from beetsplug.essentia import EssentiaInterface
 import confuse
+
+def add_media_fields(config: ConfigView):
+    mood_mapping = config['tags']['mood']['mapping'].get(str)
+    mood = mediafile.MediaField(
+        mediafile.MP3DescStorageStyle(mood_mapping),
+        mediafile.StorageStyle(mood_mapping)
+    )
+    mediafile.MediaFile.add_field('mood', mood)
+    Item._media_fields.add('mood')
 
 def do_stuff():
     config = confuse.Configuration('beets-essentia', __name__)
@@ -16,8 +27,13 @@ def do_stuff():
     handler.setLevel(logging.DEBUG)
     logger.addHandler(handler)
 
+    add_media_fields(config)
+
     es = EssentiaInterface(config, logger)
-    es.analyse([Item.from_path('/mnt/idata/local/users/auridh/Music/Picard/GOGAE.flac')])
+    gogae = Item.from_path('/mnt/idata/local/users/auridh/Music/Picard/GOGAE.flac')
+    thunder = Item.from_path('/mnt/idata/local/users/auridh/Music/Picard/THUNDER.flac')
+
+    es.analyse([gogae, thunder])
 
 if __name__ == '__main__':
     do_stuff()
